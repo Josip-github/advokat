9b111220654f59da500eed2308e1fcf58a549552
Josip-github <josipcota1@gmail.com>
2021-05-15 20:45:35 +0200

    creating tables

--- advokat.sql
+++ advokat.sql
@@ -1,3 +1,29 @@
 drop database if exists advokat;
 create database advokat;
 use advokat;
+
+#1 advokat više klijenata, 1 klijent više obrana, 1 obrana više suradnika, 1 suradnik više obrana
+
+create table klijent(
+	sifra int not null primary key auto_increment,
+	ime varchar(50),
+	prezime varchar(50)
+);
+
+create table obrana(
+	sifra int not null primary key auto_increment,
+	klijent int,
+	datum_pocetka datetime
+);
+
+create table suradnik(
+	sifra int not null primary key auto_increment,
+	ime varchar(50),
+	prezime varchar(50)
+);
+
+create table suradnik_obrana(
+	sifra int not null primary key auto_increment,
+	suradnik int,
+	obrana int
+);
